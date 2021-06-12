3b566b3b43cddb7a76ecdb2b87bd80fdead0ebd3
d.valenta <dziyana.valenta@bglgroup.co.uk>
2021-06-12 09:35:13 +0300

    just test how to use git

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -4,7 +4,7 @@ public class StringCalculator {
 
         System.out.println("TDD kata");
     }
-
+    //just test how to use git
     public int add(String numbers) {
         if (numbers.isEmpty()) return 0;
         else return Integer.parseInt(numbers);
